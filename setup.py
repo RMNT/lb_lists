198108cdbf82e24406e6a3e563e6615d1fb2d5e2
raur5443 <raminta.urbonaviciute@mif.stud.vu.lt>
2021-07-26 18:20:34 +0300

    added setup and main fuctions

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='LB Lists',
+    version='0.1.0',    
+    description='a package for Letterboxd list comparison',
+    url='https://github.com/RMNT/lb_lists',
+    author='Raminta Urbonavičiūtė',
+    author_email='urbonaviciuteraminta@gmail.com',
+    license='MIT',
+    packages=['LBLists'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy', 're', 'requests', 'bs4', 'os', 'random', 'time'                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
